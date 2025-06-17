92ca62edd15b3cc756279f707175a69f6fe9ee78
Shri Harshan <132484829+iamshriharshan@users.noreply.github.com>
2025-06-18 02:22:11 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask, render_template, request, jsonify
-from dotenv import load_dotenv
 import google.generativeai as genai
 import os
 
-
-
-API_KEY = "AIzaSyBjL_Lsy920qyyVOUip-1UC7UMlHM4O_hY"
-
-
+API_KEY = "eNter_API_Key"
 
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel("gemini-2.0-flash")
-
-
 system_prompt = (
     "You are a strict technical interviewer for software engineering roles. "
     "start the interview slowely , interact first and then start"
@@ -21,11 +14,7 @@ system_prompt = (
     "and ask the next relevant question. Cover topics like data structures, algorithms, system design, "
     "and coding logic. Keep it professional and focused."
 )
-
-
 chat = model.start_chat(history=[{"role": "user", "parts": [system_prompt]}])
-
-
 app = Flask(__name__)
 
 @app.route("/")
